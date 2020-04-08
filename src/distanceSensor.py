d01b726208e6539e6d7095f65d2b835fc3e986ba
Matheus Kunnen <matheus@matheus.laptop>
2020-04-08 15:21:12 -0400

    Melhora no controle dos motores. Leitura de distancia com o sensor ultrasonico

--- src/distanceSensor.py
+++ src/distanceSensor.py
@@ -6,5 +6,40 @@
 #####################################################
 
 class DistanceSensor:
-    def __init__(self):
-        pass
+    def __init__(self, pin_echo, pin_trigger):
+        self.pin_echo = pin_echo
+        self.pin_trigger = pin_trigger
+        self.initPinout()
+
+    def initPinout(self):
+        GPIO.setup(self.pin_trigger, GPIO.OUT)
+        GPIO.setup(self.pin_echo, GPIO.IN)
+
+    # Codigo adaptado de https://tutorials-raspberrypi.com/raspberry-pi-ultrasonic-sensor-hc-sr04/
+    # OBS: Adicionar no relatorio referencia do codigo
+    def distance(self):
+        # set Trigger to HIGH
+        GPIO.output(self.pin_trigger, True)
+    
+        # set Trigger after 0.01ms to LOW
+        time.sleep(0.00001)
+        GPIO.output(self.pin_trigger, False)
+    
+        StartTime = time.time()
+        StopTime = time.time()
+    
+        # save StartTime
+        while GPIO.input(self.pin_echo) == 0:
+            StartTime = time.time()
+    
+        # save time of arrival
+        while GPIO.input(self.pin_echo) == 1:
+            StopTime = time.time()
+    
+        # time difference between start and arrival
+        TimeElapsed = StopTime - StartTime
+        # multiply with the sonic speed (34300 cm/s)
+        # and divide by 2, because there and back
+        distance = TimeElapsed * 17500.0
+    
+        return distance
--- src/motorController.py
+++ src/motorController.py
@@ -10,21 +10,28 @@ import RPi.GPIO as GPIO
 from motor import Motor
 
 class MotorController:
+    # Direcoes possiveis 
     STOP = 0
     FORWARD = 1
     REVERSE = 2
     
+    # Velocidade predeterminadas 
     FAST = 0
     MEDIUM = 1
     SLOW = 2
+
+    # Velocidade Maxima e Minima dos motores 
+    MAX_VEL = 100
+    MIN_VEL = 40
+
+    # Constante de ajuste e sensibilidade a variacoes da direcao
+    DIR_K = 1/100
 
     def __init__(self, motors_l, motors_r):
         self.motors_l = motors_l
         self.motors_r = motors_r
         self.vel = 0
         self.setVel(self.vel)
-
-
 
     def move(self, direction):
         if (direction == self.STOP):
@@ -37,13 +44,30 @@ class MotorController:
             self.motors_l.setDirection(Motor.REVERSE)
             self.motors_r.setDirection(Motor.REVERSE)
 
-    # Deve calcular a velocidade de cada motor para girar, em funcao da 
-    # posicao 
+    # @desc: Calcula a velocidade de cada motor para girar, em funcao da posicao 
+    # @param: int pos, variacao da direcao
+    #               pos = 0 => sem variacao de velocidad e entre motores
+    #               pos > 0 => gira a direita
+    #               pos < 0 => gira a esquerda
     def direction(self, pos):
-        pass
+        self.pos = pos
+
+        vel_l = self.vel + self.pos*MotorController.DIR_K
+        if (vel_l < MotorController.MIN_VEL):
+            vel_l = MotorController.MIN_VEL
+        elif(vel_l > MotorController.MAX_VEL):
+            vel_l= MotorController.MAX_VEL
         
-    # Falta mudar para que calcule a velocidade nova de cada motor em relacao a
-    # direcao 
+        vel_r = self.vel - self.pos*MotorController.DIR_K
+        if (vel_r < MotorController.MIN_VEL):
+            vel_r = MotorController.MIN_VEL
+        elif(vel_r > MotorController.MAX_VEL):
+            vel_r= MotorController.MAX_VEL
+
+        self.motors_l.setVel(vel_l)
+        self.motors_r.setVel(vel_r)
+        
+        
     def setVel(self, vel):
         if (vel == self.FAST):
             self.vel = 90
@@ -52,5 +76,5 @@ class MotorController:
         elif (vel == self.SLOW):
             self.vel = 50
 
-        self.motors_l.setSpeed(self.vel)
-        self.motors_r.setSpeed(self.vel)
+        self.motors_l.setVel(self.motors_l.vel^2/self.vel)
+        self.motors_r.setVel(self.motors_r.vel^2/self.vel)
